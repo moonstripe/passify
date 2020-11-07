babc729bdf87d875a65c0a5bf710270cd00be891
Kojin Glick <kojinglick@gmail.com>
2020-11-07 10:51:07 -0800

    drawer skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,95 @@
 import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Navbar from './pages/common/components/Navbar';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 
 import {
-  WrappedSignUp,
-  WrappedSignIn,
+    WrappedSignUp,
+    WrappedSignIn,
 } from './pages/Viewer';
 
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    drawerContainer: {
+        overflow: 'auto',
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+    },
+}));
 
 
 function App() {
-  return (
-    <Router>
-      <Navbar/>
-      <Route path='/signup' component={WrappedSignUp}/>
-      <Route path='/signin' component={WrappedSignIn}/>
-      <Route exact path="/">
-        <h1>Welcome to the about</h1>
-      </Route>
-    </Router>
-  );
+    const classes = useStyles();
+
+    return (
+            <div className={classes.root}>
+                <Router>
+                <CssBaseline/>
+                <Drawer
+                    className={classes.drawer}
+                    variant="permanent"
+                    classes={{
+                        paper: classes.drawerPaper,
+                    }}
+                >
+                    <Toolbar/>
+                    <div className={classes.drawerContainer}>
+                        <List>
+                            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                                <ListItem button key={text}>
+                                    <ListItemIcon>{index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}</ListItemIcon>
+                                    <ListItemText primary={text}/>
+                                </ListItem>
+                            ))}
+                        </List>
+                        <Divider/>
+                        <List>
+                            {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                                <ListItem button key={text}>
+                                    <ListItemIcon>{index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}</ListItemIcon>
+                                    <ListItemText primary={text}/>
+                                </ListItem>
+                            ))}
+                        </List>
+                    </div>
+                </Drawer>
+                <main className={classes.content}>
+                    <Navbar className={classes.appBar}/>
+                    <Route path='/signup' component={WrappedSignUp}/>
+                    <Route path='/signin' component={WrappedSignIn}/>
+                    <Route exact path="/">
+                        <h1>Welcome to the about</h1>
+                    </Route>
+                </main>
+            </Router>
+            </div>
+    );
 }
 
 export default App;
