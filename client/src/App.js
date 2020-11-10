b49986386d1007b4fa612a24695b7ca2e0c562a8
Allie Bauer <alliebauer2@gmail.com>
2020-11-09 18:25:38 -0800

    add footer with pw generator

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import StickyFooter from './pages/common/components/Footer';
 import Navbar from './pages/common/components/Navbar';
 
 import {
@@ -18,6 +19,7 @@ function App() {
       <Route exact path="/">
         <h1>Welcome to the about</h1>
       </Route>
+      <StickyFooter />
     </Router>
   );
 }
---
+++ client/src/pages/common/components/Footer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Generator from './Generator';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+  },
+  main: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(2),
+  },
+  footer: {
+    padding: theme.spacing(3, 2),
+    marginTop: 'auto',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+  },
+}));
+
+export default function StickyFooter() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <footer className={classes.footer}>
+        <Container maxWidth="sm">
+          <Generator />
+        </Container>
+      </footer>
+    </div>
+  );
+}
