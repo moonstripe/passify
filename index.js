8763a2f5ee9502dbfac3d79f31648546338e3b8d
ReverbDesigns <jordymcbell@gmail.com>
2020-11-20 12:32:40 -0800

    change productio root for deploy

--- index.js
+++ index.js
@@ -6,17 +6,18 @@ const routes = require('./routes');
 require('./services/passport');
 
 const app = express();
-
+const publicPath = path.join(__dirname, '..', 'public');
 const PORT = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
+app.use(express.static(publicPath));
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(routes);
 
 app.listen(PORT, () => {
-  console.log('Server started listening on PORT http://localhost:3001');
+  console.log('Server is up!');
 });
